bf9589831615af176812bca5ad95c3375a7daf31
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-03-16 19:14:11 -0400

    Layout revision

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-//1. add featured class
+//1. Add featured class
 let firstS = document.querySelector("section")
 firstS.classList.add("featured")
+// 2. Add another article post to the section
 
+// Creating The  Elements & Inner Text Content
     let article = document.createElement("article")
-    
-    
-    let image = document.createElement("img")
 
+    let image = document.createElement("img")
         image.setAttribute("src","./images/paul-gilmore-unsplash.jpg")
         image.alt="Image of a mountain in front of a lake."   
-    
     
     let header = document.createElement("h3")
         header.textContent = "Stop Planning"
@@ -17,34 +16,36 @@ firstS.classList.add("featured")
     let paragraph = document.createElement("p")
         paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
     
-    
     let aside = document.createElement("aside")
     
-    let p = document.createElement("p")
+    let p = document.createElement("p") // second paragraph tag
+
     let span = document.createElement("span")
-    span.textContent = "4 Minutes"
+        span.textContent = " 4 Minutes" //spaces matter
+
     let strong = document.createElement("strong")
-    strong.textContent = "Read Time:"
+        strong.textContent = "Read Time:"
+
     let a = document.createElement("a")
-    a.setAttribute("href", "#")
-    a.textContent = "Read more..."
+        a.setAttribute("href", "#")
+        a.textContent = "Read more..."
     
-    aside.append(p)
+    // Appending To The HTML
+    aside.append(p) //
+
+    p.append(strong, span, a)
     
-    p.append(span, a)
-    
-    span.append(strong)
-    
-    
+    // strong.append(span)
+    span.prepend(strong)
+
     article.append(image, header, paragraph, aside)
     
-    
+    // 3. Switch first and second posts
     let postsSection = document.querySelector(".posts")
     postsSection.append(article)
     let firstArticle = postsSection.querySelector("article")
     let secondArticle = firstArticle.nextElementSibling
-    //console.log(articles)
-    secondArticle.prepend(firstArticle)
-    //let secondArticle = firstArticle.nextElementSibling
-    console.log(secondArticle)
-   
+    // secondArticle.prepend(firstArticle)
+    firstArticle.before(secondArticle)
+
+    
