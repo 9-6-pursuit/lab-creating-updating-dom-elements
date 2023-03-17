b991fc296338c2af959b0f15715677877c7b89a5
amshepard <antoineshepard@pursuit.org>
2023-03-16 20:01:35 -0400

    adding innerHTML notes for future reference.

--- main.js
+++ main.js
@@ -30,6 +30,15 @@ let twoToOne = document.querySelectorAll("section.posts article")
 twoToOne[1].after(twoToOne[0])
 
 
+// In JavaScript, the ability to interact with and manipulate the content of HTML elements is a crucial aspect of web development. Two of the most commonly used properties for accessing and modifying content in web pages are `innerText` and `innerHTML`. Although these properties have distinct differences, they share several similarities that make them both useful tools for developers.
+
+// The `innerText` property is used to access or replace the text content of an HTML element, while the `innerHTML` property is used to access or replace the HTML content of an element, including any child elements and their HTML tags.
+
+// While `innerText` and `innerHTML` have different functional purposes, they share several key similarities. Both properties allow developers to access and manipulate the content of HTML elements, and both can be used to retrieve the content of an element for use in other parts of a JavaScript program. Additionally, both `innerText` and `innerHTML` can be used to change the text or HTML content of an element, allowing developers to dynamically update the content of web pages in response to user actions or events.
+
+// One important consideration when using `innerText` and `innerHTML` is that they can have different performance characteristics. Because `innerHTML` returns the entire HTML content of an element, including any child elements, it can be slower to execute than `innerText`, which returns only the text content of an element. However, in certain situations, the ability to access or replace the HTML content of an element may be necessary, making `innerHTML` the better choice.
+
+
 
 
 
