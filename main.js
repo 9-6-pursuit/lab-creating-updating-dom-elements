d96d46dce53f368849eae01719900c4246aa17ee
Raymond <raymondjohnson@pursuit.org>
2023-03-19 08:56:07 -0400

    not yet fully passing tests

---
+++ main.js
@@ -0,0 +1,47 @@
+window.onload = () => {
+  let firstSection = document.querySelector('section');
+  firstSection.setAttribute('class', 'featured');
+  let article = document.createElement('article');
+  
+  let postSection = document.querySelector('main > .posts');
+  
+  let newImage = document.createElement('img')
+  newImage.src = "./images/paul-gilmore-unsplash.jpg";
+  newImage.alg = "Image of a mountain in front of a lake.";
+  
+  let articleHeader = document.createElement('h3');
+  articleHeader.innerText = "Stop Planning";
+  
+  let paragraph = document.createElement('p');
+  paragraph.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?`;
+  
+  let aside = document.createElement('aside');
+  let asideParagraph = document.createElement('p');
+  
+  let span = document.createElement('span');
+
+  let bold = document.createElement('strong');
+  
+  bold.innerText = 'Read Time:'
+  span.append(bold)
+  span.innerText += ' 4 Minutes'
+
+  asideParagraph.append(span)
+  console.log(asideParagraph)
+  aside.append(asideParagraph)
+  article.append(newImage, articleHeader, paragraph, aside)
+  postSection.append(article)
+  
+  let firstPostArticle = document.querySelector('.posts > article');
+  firstPostArticle.remove;
+  postSection.append(firstPostArticle)
+}
+
+
+/*
+let planetCard = document.createElement('li');
+//planetCard.classList.add("planet-card")
+//planetCard.className = 'planet-card'
+planetCard.setAttribute('class',"planet-card");
+let image = document.createElement('img');
+*/
